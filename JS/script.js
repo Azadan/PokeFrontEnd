e71dc548cc0940804e2035002e289de850539a21
Azadan <azadan@live.com>
2025-01-30 23:55:44 +0100

    Issue #7 : Post a captures pokemon to the backend

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,28 @@
-import { consolelog, generateRandomPokemon, displayGeneratedPokemon} from './ModulesAPI/PokeApi.js';
+import { consolelog, generateRandomPokemon, displayGeneratedPokemon, fetchCatchAPokemon, postCatchAPokemon, apiEndpoint} from './ModulesAPI/PokeApi.js';
 const generatePoke = document.getElementById("generatePoke");
 const catchContainer = document.getElementById("catchContainer");
-
 consolelog();
 
-generatePoke.addEventListener("click", () => {
-    generateRandomPokemon();
+generatePoke.addEventListener("click", async () => {
+    const pokeData = await generateRandomPokemon();
+    
 
-    let capturePokemon = document.getElementById("capturePokemonButton");
+   let capturePokemon = document.getElementById("capturePokemonButton");
     if (!capturePokemon) {
         capturePokemon = document.createElement("button");
         capturePokemon.id = "capturePokemonButton"; 
         capturePokemon.innerText = "Fånga Pokemon";
         catchContainer.appendChild(capturePokemon);
 
-    }
+        capturePokemon.addEventListener("click", () => {
+            postCatchAPokemon(pokeData)
+            .then(response => {
+                console.log('Fångat Pokémon', response)
+            })
+            .catch(error => {
+                console.error('Misslyckats att fånga Pokémon: ', error);
+            })
+        })
+    } 
 })
+
